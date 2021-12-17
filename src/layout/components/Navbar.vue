321039edd7351c234e9d34dbf70623d5e5f40623
shenwenjie <15805199474@163.com>
2021-12-17 13:14:50 +0800

    fix:change ui

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -8,24 +8,6 @@
       <!-- <search id="header-search" class="right-menu-item" /> -->
 
       <screenfull id="screenfull" class="right-menu-item hover-effect" />
-
-      <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
-        <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
-          <i class="el-icon-caret-bottom" />
-        </div>
-        <el-dropdown-menu slot="dropdown">
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
-          <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
-          </el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
     </div>
   </div>
 </template>
